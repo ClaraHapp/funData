b5333b976b717e2a7f29d95610b660b8034d6c03
ClaraHapp <clara.happ@stat.uni-muenchen.de>
2015-11-26 14:56:59 +0100

    changed xVal to argvals

--- R/irreg.R
+++ R/irreg.R
@@ -29,16 +29,16 @@
 
 
 
-plot.xVals <- function(x, y, type = "p", pch = 20,
-                       col =rainbow(nObs(x)), xlab = "xVal", ylab = "",
-                       add = FALSE, xlim = range(x@xVal), ylim = c(1,nObs(x)), ...)
+plot.argvalss <- function(x, y, type = "p", pch = 20,
+                       col =rainbow(nObs(x)), xlab = "argvals", ylab = "",
+                       add = FALSE, xlim = range(x@argvals), ylim = c(1,nObs(x)), ...)
 {
   
   plot(x = NULL, y = NULL, type = "n", xlim = xlim, ylim = ylim, xlab= xlab, ylab = ylab, ...)
   
   
   for(i in 1:nObs(x))
-    points(x = x@xVal[[i]], y = rep(i, length(x@xVal[[i]])), type = type, pch = pch, col = col[i], ...)
+    points(x = x@argvals[[i]], y = rep(i, length(x@argvals[[i]])), type = type, pch = pch, col = col[i], ...)
   
 }
 
--- tests/testthat/test_funDataClass.R
+++ tests/testthat/test_funDataClass.R
@@ -2,32 +2,32 @@ context("Testing class definitions")
 
 test_that("funData class constructor", {
   # validity
-  expect_error(funData(xVal = "Test", X = matrix(2, nrow = 1)),
-               "unable to find an inherited method") # xVal is neither list nor vector of numerics
-  expect_error(funData(xVal = list(1,"Test"), X = array(1:2, dim = c(1,1,1))),
-               "all xVal elements must be numeric") # xVal contains non-numeric
-  expect_error(funData(xVal = list(1:5), X = list(cbind(1:5,6:10))), 
+  expect_error(funData(argvals = "Test", X = matrix(2, nrow = 1)),
+               "unable to find an inherited method") # argvals is neither list nor vector of numerics
+  expect_error(funData(argvals = list(1,"Test"), X = array(1:2, dim = c(1,1,1))),
+               "all argvals elements must be numeric") # argvals contains non-numeric
+  expect_error(funData(argvals = list(1:5), X = list(cbind(1:5,6:10))), 
                "unable to find an inherited method") # X is not an array
-  expect_error(funData(xVal = list(1:5), X = array(1:25, dim = c(1,5,5))), 
-               "xVal and X element have different support dimensions! X-Dimensions must be of the form N x M1 x ... x Md") # X has wrong dimensional support
-  expect_error(funData(xVal = list(1:5, 1:4), X = array(1:25, dim = c(1,5,5))),
-               "xVal and X have different number of sampling points! X-Dimensions must be of the form N x M1 x ... x Md") # X has wrong dimension (sampling points)
+  expect_error(funData(argvals = list(1:5), X = array(1:25, dim = c(1,5,5))), 
+               "argvals and X element have different support dimensions! X-Dimensions must be of the form N x M1 x ... x Md") # X has wrong dimensional support
+  expect_error(funData(argvals = list(1:5, 1:4), X = array(1:25, dim = c(1,5,5))),
+               "argvals and X have different number of sampling points! X-Dimensions must be of the form N x M1 x ... x Md") # X has wrong dimension (sampling points)
   
   # acception of NA values
-  expect_is(funData(xVal = c(1:5), X = matrix(c(1:19, NA), nrow = 4)), "funData")
+  expect_is(funData(argvals = c(1:5), X = matrix(c(1:19, NA), nrow = 4)), "funData")
   
   # special cases: one-dimensional domain
-  expect_equal(funData(xVal = 1:5, X = matrix(1:20, nrow = 4)), funData(xVal = list(1:5), X = matrix(1:20, nrow = 4))) 
+  expect_equal(funData(argvals = 1:5, X = matrix(1:20, nrow = 4)), funData(argvals = list(1:5), X = matrix(1:20, nrow = 4))) 
 })
 
 
 test_that("multiFunData class constructor", {
-  f1 <- funData(xVal = 1:5, X = matrix(1:20, nrow = 4))
+  f1 <- funData(argvals = 1:5, X = matrix(1:20, nrow = 4))
   
   # validity
   expect_error(multiFunData(list(5, 5)), 
                "elements of multiFunData must be of class funData!")
-  expect_error(multiFunData(list(f1, funData(xVal = list(1:5, 6:10), X = array(1:125, c(5,5,5))))),
+  expect_error(multiFunData(list(f1, funData(argvals = list(1:5, 6:10), X = array(1:125, c(5,5,5))))),
                "All elements must have the same number of observations!")
   
   # conversion
@@ -40,20 +40,20 @@ test_that("multiFunData class constructor", {
 
 test_that("irregfunData class constructor", {
   # Validity:
-  # xVal
-  expect_error(irregFunData(xVal = "Test", X = list(5)),
-               "unable to find an inherited method") # xVal is no list
-  expect_error(irregFunData(xVal = list("Test"), X = list(5)),
-               "xVal must be supplied as list of numerics")  # xVal is no list of numerics
+  # argvals
+  expect_error(irregFunData(argvals = "Test", X = list(5)),
+               "unable to find an inherited method") # argvals is no list
+  expect_error(irregFunData(argvals = list("Test"), X = list(5)),
+               "argvals must be supplied as list of numerics")  # argvals is no list of numerics
   # X
-  expect_error(irregFunData(xVal = list(5), X = "Test"),
+  expect_error(irregFunData(argvals = list(5), X = "Test"),
                "unable to find an inherited method") # X is no list
-  expect_error(irregFunData(xVal = list(5), X = list("Test")),
+  expect_error(irregFunData(argvals = list(5), X = list("Test")),
                "X must be supplied as list of numerics") # X is no list of numerics
-  # relation between xVal and X
-  expect_error(irregFunData(xVal = list(1:5), X = list(1:5, 2:4)),
-               "Different number of observations for xVal and X")
-  expect_error(irregFunData(xVal = list(1:5, 1:4), X = list(1:5, 2:4)),
-               "Different numbers of observation points in xVal and X")
+  # relation between argvals and X
+  expect_error(irregFunData(argvals = list(1:5), X = list(1:5, 2:4)),
+               "Different number of observations for argvals and X")
+  expect_error(irregFunData(argvals = list(1:5, 1:4), X = list(1:5, 2:4)),
+               "Different numbers of observation points in argvals and X")
 })
 
