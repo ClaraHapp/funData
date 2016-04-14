7b0686e9d890f90c35349db49eb6e7a398756a8f
ClaraHapp <clara.happ@stat.uni-muenchen.de>
2016-04-14 10:58:45 +0200

    as.funData (for irregFunData objects) now with examples

--- R/funDataClass.R
+++ R/funDataClass.R
@@ -445,6 +445,20 @@ setMethod("irregFunData", signature = c(argvals = "list", X = "list"),
 #' @name irregFunData-setAs
 #'   
 #' @keywords internal
+#' 
+#' @examples
+#' # create irregFunData object with 2 observations
+#' i1 <- irregFunData(argvals = list(1:5, 3:6), X = list(2:6, 4:7))
+#' i1@@argvals # argvals and X are both lists with 2 entries
+#' i1@@X
+#' 
+#' # coerce to funData object (with missing values)
+#' f1 <- as.funData(i1)
+#' # argvals is a list containing one vector
+#' # (one-dimensional domain, union of all observation points)
+#' f1@@argvals
+#' # X is a matrix with 2 rows and missing values
+#' f1@@X
 setAs("irregFunData", "funData", 
       def = function(from){      
         #     if(dimSupp(from) > 1)
