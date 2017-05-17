d41a9d53aabb5a9856470787d479d790c34b0108
ClaraHapp <clara.happ@stat.uni-muenchen.de>
2017-05-17 23:16:18 +0200

    Test ggplot methods

--- tests/testthat/test_plotMethods.R
+++ tests/testthat/test_plotMethods.R
@@ -44,5 +44,34 @@ test_that("plots", {
   
   expect_null(plot(as.irregFunData(object1D)))
   expect_null(plot(as.irregFunData(object1D), col = c("green", "red")))
+})
+
+
+test_that("ggplots", {
+  # check errors
+  expect_error(ggplot(funData(argvals = list(1:5,2:6,3:7), X = array(10*5*5*5, dim = c(10,5,5,5)))),
+               "plot is implemented only for functional data with one- or two-dimensional domain")
+  expect_error(ggplot(funData(argvals = list(1:5,2:6), X = array(10*5*5, dim = c(10,5,5))), obs = 1:2),
+               "plot: specify one observation for plotting")
   
+  # check functionality
+  argvals <- seq(0,2*pi,0.01)
+  # 1D
+  object1D <- funData(argvals, outer(seq(0.75, 1.25, length.out = 11), sin(argvals)))
+  expect_s3_class(ggplot(object1D), "ggplot")
+  
+  # 2D
+  X <- array(0, dim = c(2, length(argvals), length(argvals)))
+  X[1,,] <- outer(argvals, argvals, function(x,y){sin((x-pi)^2 + (y-pi)^2)})
+  X[2,,] <- outer(argvals, argvals, function(x,y){sin(2*x*pi) * cos(2*y*pi)})
+  object2D <- funData(list(argvals, argvals), X)
+  
+  expect_s3_class(ggplot(object2D, obs = 1), "ggplot")
+   
+  # multivariate
+  expect_equal(length(ggplot(multiFunData(object1D, 2*object1D), plotGrid = TRUE)), 2)
+  expect_equal(length(ggplot(multiFunData(object1D, 2*object1D), plotGrid = FALSE)), 2)
+  
+  # irreg
+  expect_s3_class(ggplot(as.irregFunData(object1D)), "ggplot")
 })
