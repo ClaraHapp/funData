a84ef97b4f868fc424b10e0c028a4624ad745df3
C. Happ <clara.happ@stat.uni-muenchen.de>
2018-08-06 12:27:31 +0200

    Update docu for argvals() / X()

--- R/get_set.R
+++ R/get_set.R
@@ -5,35 +5,35 @@
 #'
 #' Objects of class \code{funData} or \code{irregFunData} have two slots,
 #' \code{argvals} (for the x-values) and \code{X} (for the y-values for
-#' each observation). Using the \code{argvals} (alias: \code{argvals})
-#' and \code{X} (alias: \code{X}) methods for the classes
+#' each observation). Using the \code{argvals} (alias: \code{getArgvals})
+#' and \code{X} (alias: \code{getX}) methods for the classes
 #' \code{funData} and \code{irregFunData} is equivalent to accessing the
 #' slots directly via \code{object@@argvals} and \code{object@@X}.
 #' Analogously, the \code{argvals<-} and \code{X<-} functions are
-#' equivalent to setting \code{object@@argvals} to \code{newArgvals} or
-#' \code{object@@X} to \code{newX}, respectively. The new values must
+#' equivalent to setting \code{object@@argvals} to \code{value} or
+#' \code{object@@X} to \code{value}, respectively. The new values must
 #' hence have the same structure as the original ones. As an exception,
-#' for an object of class \code{funData} the number of new observations in
-#' \code{newX} may differ from the current (e.g. when adding new
-#' observations). In this case, the function throws a warning.
+#' for an object of class \code{funData} the number of new X values may
+#' differ from the current (e.g. when adding new observations). In this
+#' case, the function throws a warning.
 #'
 #' Objects of class \code{multiFunData} are lists of several
-#' \code{funData} objects. The functions \code{argvals} and \code{X}
-#' for \code{multiFunData} objects therefore return a list of the same
-#' length as \code{object}, where each list element corresponds to the
+#' \code{funData} objects. The functions \code{argvals} and \code{X} for
+#' \code{multiFunData} objects therefore return a list of the same length
+#' as \code{object}, where each list element corresponds to the
 #' \code{argvals} or \code{X} slot of the univariate element. The
-#' \code{argvals<-} and \code{argvals} functions for
-#' \code{multiFunData} objects must be lists of the same length as
-#' \code{object}, where each list element corresponds to the new
-#' \code{argvals} or new \code{X} slot for the univariate elements.
+#' \code{argvals<-} and \code{X<-} functions for \code{multiFunData}
+#' objects must receive lists of the same length as \code{object}, where
+#' each list element corresponds to the new \code{argvals} or new \code{X}
+#' slot for the univariate elements.
 #'
-#' For all classes, the set functions do not change the object, unless
-#' their result is assigned to \code{object} (see Examples).
+#' @section Warning: The functions \code{getArgvals} / \code{getX} and
+#'   \code{setArgvals} / \code{setX} from former package versions are
+#'   deprecated. use \code{argvals} and \code{X} instead.
 #'
 #' @param object An object of class \code{funData}, \code{irregFunData} or
 #'   \code{multiFunData}.
-#' @param newArgvals See Details.
-#' @param newX See Details.
+#' @param value New \code{argvals} or \code{X}. See Details.
 #'
 #' @return See Details.
 #'
@@ -51,19 +51,13 @@
 #' argvals(object)
 #' X(object)
 #'
-#' # aliases
-#' argvals(object)
-#' X(object)
-#'
 #' # set-methods
-#' argvals<-(object, 0:4)
-#' object # no change
-#' object <- argvals<-(object, 0:4) # reassign the result to object
-#' object # now, argvals is changed
-#' \dontrun{object <- argvals<-(object, 1:4)} # wrong length
-#' object <- X<-(object, rbind(0:4, 5:9))
-#' newObject <- X<-(object, rbind(0:4, 5:9, 10:14)) # warning: now 3 observations (was 2 before)
-#' \dontrun{object <- X<-(object, rbind(1:4, 5:8))} # wrong length
+#' argvals(object) <- 0:4
+#' object 
+#' \dontrun{argvals(object) <- 1:4} # wrong length
+#' X(object) <- rbind(0:4, 5:9)
+#' \dontrun{X(object) <- rbind(0:4, 5:9, 10:14) # warning: now 3 observations (was 2 before)}
+#' \dontrun{X(object) <- rbind(1:4, 5:8)} # wrong length
 #'
 #' ### Univariate (irregular)
 #' irregObject <- irregFunData(argvals = list(1:5, 2:4), X = list(2:6, 3:5))
@@ -73,8 +67,9 @@
 #' argvals(irregObject)
 #' X(irregObject)
 #'
-#' newIrregObject <- argvals<-(irregObject, list(0:4, 1:3))
-#' newIrregObject <- X<-(irregObject, list(12:16, 13:15))
+#'# set-methods
+#' argvals(irregObject) <- list(0:4, 1:3)
+#' X(irregObject) <- list(12:16, 13:15)
 #'
 #' ### Multivariate
 #' multiObject <- multiFunData(object, funData(argvals = 1:3, X = rbind(3:5, 6:8)))
@@ -85,8 +80,8 @@
 #' X(multiObject)
 #'
 #' # set-methods (for special cases see univariate version)
-#' multiObject <- argvals<-(multiObject, list(5:1, 3:1))
-#' multiObject <- X<-(multiObject, list(rbind(5:1, 10:6), rbind(5:3, 8:6)))
+#' argvals(multiObject) <- list(5:1, 3:1)
+#' X(multiObject) <- list(rbind(5:1, 10:6), rbind(5:3, 8:6))
 setGeneric("argvals", function(object) {standardGeneric("argvals")})
 
 #' Get argvals slot for funData objects
